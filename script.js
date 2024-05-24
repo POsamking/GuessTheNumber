6636bcfb0c241d9c3a6a51b7e0edc50f5eedbb37
Prince <168102758+POsamking@users.noreply.github.com>
2024-05-24 19:54:42 +0100

    First Commit
    Date: 24/05/2024

---
+++ script.js
@@ -0,0 +1,24 @@
+let randomNumber = Math.floor(Math.random() * 2) + 1;
+
+function checkGuess() {
+    let userGuess = document.getElementById('guessInput').value;
+    let resultElement = document.getElementById('result');
+
+    if(userGuess == randomNumber) {
+        resultElement.style.color = 'green';
+        resultElement.textContent = 'Congratulations! You guessed the number.';
+    } else {
+        resultElement.style.color = 'red';
+        resultElement.textContent = 'Sorry, that\'  s not correct. Try again.';
+    }
+}
+
+document.getElementById ('resetButton').addEventListener('click', function() {
+resetGame();
+});
+
+function resetGame () {
+    randomNumber = Math, floor(math.random()* 20)+1;
+    document.getElementById('guessInput').value = '';
+    document.getElementById('result').textContent ='';
+}
